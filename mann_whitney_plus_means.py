0e3d7b36bc0b29abf7daad4b7b4737c0962574d4
shmuelnaaman <shmuel.naaman@yahoo.com>
2014-12-08 13:33:39 -0500

    Add the function that calculate the max Temp during fuggy and non foggy days

---
+++ mann_whitney_plus_means.py
@@ -0,0 +1,31 @@
+import pandas
+import pandasql
+
+
+def max_temp_aggregate_by_fog(filename):
+    '''
+    This function run a SQL query on a dataframe of
+    weather data.  The SQL query  return two columns and
+    two rows - whether it was foggy or not (0 or 1) and the max
+    maxtempi for that fog value (i.e., the maximum max temperature
+    for both foggy and non-foggy days).  
+    
+       
+    You can see the weather data below:
+    https://www.dropbox.com/s/7sf0yqc9ykpq3w8/weather_underground.csv
+    '''
+    weather_data = pandas.read_csv(filename)
+
+    q = """
+    SELECT fog,  max(cast (maxtempi as integer)) 
+    FROM weather_data 
+    WHERE cast(fog as integer) = 0 
+    OR cast(fog as integer) = 1
+    GROUP BY fog 
+    
+    """
+    
+    #Execute your SQL command against the pandas frame
+    Max_temp_fog = pandasql.sqldf(q.lower(), locals())
+
+    return Max_temp_fog
