7f752c35ef7c6898cb7e88fbe27addba678ecf56
shmuelnaaman <shmuel.naaman@yahoo.com>
2014-12-08 14:39:49 -0500

    Add the function that calculate the Average Min Temp on rainy days

---
+++ avg_mean_temperature.py
@@ -0,0 +1,28 @@
+import pandas
+import pandasql
+
+def avg_mean_temperature(filename):
+    '''
+    This function run a SQL query on a dataframe of
+    weather data.  The SQL query return  the average 
+    meantempi on days that are a Saturday
+    or Sunday (i.e., the the average mean temperature on weekends).
+    
+    
+    Here I convert dates to days of the week via the 'strftime' keyword in SQL.
+    
+    You can see the weather data below:
+    https://www.dropbox.com/s/7sf0yqc9ykpq3w8/weather_underground.csv
+    '''
+    weather_data = pandas.read_csv(filename)
+
+    q = """
+    SELECT  avg(cast (meantempi as integer)) 
+    FROM weather_data 
+    WHERE cast(strftime('%w', date) as integer) IN(0,6)
+    GROUP BY cast(strftime('%w', date) as integer) IN(0,6)
+    """
+    
+    #Execute your SQL command against the pandas frame
+    mean_temp_weekends = pandasql.sqldf(q.lower(), locals())
+    return mean_temp_weekends
--- avg_min_temperature.py
+++ avg_min_temperature.py
@@ -3,24 +3,29 @@ import pandasql
 
 def avg_min_temperature(filename):
     '''
-    This function run a SQL query on a dataframe of
-    weather data.  The SQL query return  the average 
-    meantempi on days that are a Saturday
-    or Sunday (i.e., the the average mean temperature on weekends).
+    This function should run a SQL query on a dataframe of
+    weather data. More specifically you want to find the average
+    minimum temperature on rainy days where the minimum temperature
+    is greater than 55 degrees.
     
+    You might also find that interpreting numbers as integers or floats may not
+    work initially.  In order to get around this issue, it may be equal to cast
+    these numbers as integers.  This can be done by writing cast(column as integer).
+    So for example, if we wanted to cast the maxtempi column as an integer, we would actually
+    write something like where cast(maxtempi as integer) = 76, as opposed to simply 
+    where maxtempi = 76.
     
-    Here I convert dates to days of the week via the 'strftime' keyword in SQL.
-    
-    You can see the weather data below:
+    You can see the weather data that we are passing in below:
     https://www.dropbox.com/s/7sf0yqc9ykpq3w8/weather_underground.csv
     '''
     weather_data = pandas.read_csv(filename)
 
     q = """
-    SELECT  avg(cast (meantempi as integer)) 
+    SELECT   avg(cast (mintempi as integer)) 
     FROM weather_data 
-    WHERE cast(strftime('%w', date) as integer) IN(0,6)
-    GROUP BY cast(strftime('%w', date) as integer) IN(0,6)
+    WHERE cast(rain as integer) = 1
+    AND cast(mintempi as integer) > 55
+    GROUP BY cast(rain as integer) = 1    AND cast(mintempi as integer) > 55
     """
     
     #Execute your SQL command against the pandas frame
